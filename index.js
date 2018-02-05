2c77daf5e22830740814c8c8f254cf414f48b098
Ryan Ghaida <ryaboug@gmail.com>
2018-02-05 17:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {dinner: "chicken and rice"}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]:value})
-  return Object
+  return Obj
 }
 
 
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
